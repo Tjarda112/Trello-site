4454313b372930e9c2b82519ad0d99dbbf25f3dd
Tjarda112 <tjardavoorn@hotmail.com>
2018-04-04 11:35:59 +0200

    Jquery added

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ app.use('/', router);
 // set static files (css and images) Location
 app.use(express.static(__dirname + '/public'));
 
+/*
 var Trello = require("node-trello");
 var t = new Trello("7877dd27f5b9b34fc38e734dadeeff61", "94260ec7eb53519bddd84cef66175f225d83fc2e8ac4d6ee616e28da519a67cd");
- /*
+ 
 t.get("/1/members/me", function(err, data) {
   if (err) throw err;
   console.log(data);
